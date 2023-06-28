80ea78d56884de3e8e691657cc0d6abe7bc5717a
Bubbles <hxhbubble@gmail.com>
2023-06-28 22:13:47 +0530

    Reconfigure

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://hxhbubble.github.io/myportfolio/',
+  base:'/myportfolio/',
   plugins: [react()],
 })
